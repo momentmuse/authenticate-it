b3cb8b4626db08d943a084fc726fc8e2c732145a
Amy Kirasack <amykirasack@gmail.com>
2020-06-06 17:36:16 +0200

    chore: cleanup

--- server/session/controllers/user.js
+++ server/session/controllers/user.js
@@ -13,25 +13,19 @@ const create = async (req, res) => {
   try {
     const user = await newUser.save();
     req.session.uid = user._id;
-    console.log('whats in the req session id', req.session.id);
-    console.log('whats in the req uid', req.session.uid);
     res.status(201).send(user);
   } catch (error) {
-    console.log(error);
     res.status(400).send(error);
   }
 };
 
 const login = async (req, res) => {
-  console.log('whats the session ', req.session.cookie);
   const { email, password } = req.body;
   const user = await User.findOne({ email: email });
   if (!user) return res.status(404).send('User does not exist');
   const validatedPass = await bcrypt.compare(password, user.password);
   if (validatedPass) {
     req.session.uid = user._id;
-    console.log('whats in the req session id', req.session.id);
-    console.log('whats in the req uid', req.session.uid);
     res.status(200).send(user);
   } else {
     res.status(401).send('Passwords do not match');
@@ -39,16 +33,13 @@ const login = async (req, res) => {
 };
 
 const profile = async (req, res) => {
-  console.log(' req session: ', req.session);
   const { uid } = req.session;
-  console.log('userId from req session: ', uid);
   try {
     console.log('again ', uid);
     const user = await User.findOne(
       { _id: uid },
       { firstName: 1, lastName: 1 }
     );
-    console.log('found the user by id ', user);
     res.status(201).send(user);
   } catch {
     console.log(error);
@@ -59,11 +50,9 @@ const profile = async (req, res) => {
 const logout = (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      console.log('😞 There was an error logging out');
       res.sendStatus(500);
     } else {
       res.clearCookie('sid');
-      console.log('🍪 Destroyed the session cookie');
       res.sendStatus(200);
     }
   });
